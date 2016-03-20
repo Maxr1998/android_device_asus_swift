2cb9168b2856afe0b4125a7be2ad4c160c8f1f18
J <joeykrim@gmail.com>
2016-03-20 16:30:17 -0700

    Updated sparrow references to wren
    Added tag to check whether required for asus/anthias

--- device.mk
+++ device.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product-if-exists, vendor/asus/anthias/anthias-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/anthias/anthias-vendor.mk) #Confirm is required?
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/sparrow/overlay
+DEVICE_PACKAGE_OVERLAYS += device/asus/wren/overlay
 
-LOCAL_PATH := device/asus/sparrow
+LOCAL_PATH := device/asus/wren
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -14,10 +14,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.sparrow:root/fstab.sparrow \
+    $(LOCAL_PATH)/fstab.wren:root/fstab.wren \
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_sparrow
-PRODUCT_DEVICE := sparrow
+PRODUCT_NAME := full_wren
+PRODUCT_DEVICE := wren
